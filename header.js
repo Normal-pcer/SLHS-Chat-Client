7d69eb86e8606e810d8789bd2a6d2a6a4b23b51d
normal-pcer <normal-pcer@outlook.com>
2023-06-10 20:24:22 +0800

    修复一个构建时的bug

--- header.js
+++ header.js
@@ -2,7 +2,8 @@ var config = undefined
 
 function getConfig() {
     if (config !== undefined) return config
-    let filename = './config.json'
+    let path = require('path')
+    let filename = path.join(__dirname, './config.json')
     let fs = require('fs')
     let fileData = fs.readFileSync(filename).toString()
     let obj = JSON.parse(fileData)
@@ -12,15 +13,16 @@ function getConfig() {
 
 function getResources() {
     let fs = require('fs')
+    let path = require('path')
 
-    let indexname = './recoursepack/index.json'
+    let indexname = path.join(__dirname, './recoursepack/index.json')
     let indexdata = JSON.parse(fs.readFileSync(indexname).toString())
 
     let filename =
         './recoursepack/' +
         indexdata[getConfig()['resource']['usedResourcePack']] +
         '/index.json'
-    let fileData = fs.readFileSync(filename)
+    let fileData = fs.readFileSync(path.join(__dirname, filename))
 
     return JSON.parse(fileData.toString())
 }
@@ -29,7 +31,7 @@ function getSingleResource(key) {
     let fs = require('fs')
     let path = require('path')
 
-    let indexname = './recoursepack/index.json'
+    let indexname = path.join(__dirname, './recoursepack/index.json')
     let indexdata = JSON.parse(fs.readFileSync(indexname).toString())
 
     let pathprev = path.join(
